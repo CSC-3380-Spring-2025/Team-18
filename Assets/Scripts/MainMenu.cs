94c08214f1e039bbfc5405a809ea2bbd19a26740
ArkHavens <jsmi821@lsu.edu>
2025-03-07 13:58:01 -0600

    Character code update, main menu visuals
    Streamlining character code,

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,66 @@
+using Godot;
+using System;
+using Game.Assets.Scripts.Loading;
+using Game.Assets.Scripts.Saving;
+
+public partial class MainMenu : CanvasLayer
+{
+	private bool _isFirstLoad = true;
+	// Called when the node enters the scene tree for the first time.
+	///Makes it so the play and exit button both function as intended when esc is pressed or game is 'paused'.
+	public override void _Ready(){
+		ProcessMode = ProcessModeEnum.Always;
+	}
+	
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	///set the visibility of the main menu screen to true, as well as call the GetTree.Paused to pause the game when 'esc' is pressed.
+	public override void _Process(double delta){
+		if(Input.IsActionJustPressed("ui_cancel")){
+			Visible = true; 
+			GetTree().Paused = true;
+		}
+	}
+	///Creates the functionality of the Play button when pressed
+	public void OnPlayPressed(){
+		Visible = false;
+		GetTree().Paused = false;
+		
+		var mainChildren = GetParent().GetChildren();
+		foreach(var child in mainChildren){
+			//If statement is so whenever the start button is pressed, it doesn't continously create a new node of Player scene
+			if(child.Name == "Player")
+				return;
+		}
+		
+		//Once the button is clicked, it moves the current scene (main menu) to the Player scene
+		var gameScene = GD.Load<PackedScene>("res://Scenes//Player.tscn");
+		var gameSceneNode = gameScene.Instantiate();
+		GetParent().AddChild(gameSceneNode);
+		// Load elements if it is the first load and they are loadable
+		if (_isFirstLoad)
+		{
+			foreach (var child in GetParent().GetChildren())
+			{
+				if (child is Loadable loadable)
+				{
+					loadable.Load();
+				}
+			}
+
+			_isFirstLoad = false;
+		}
+		
+	}
+	///Functionality for the exit button
+	public void OnExitPressed(){
+		// Save any savables on exit
+		foreach (var child in GetParent().GetChildren())
+		{
+			if (child is Savable savable)
+			{
+				savable.Save();
+			}
+		}
+		GetTree().Quit();
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,134 @@
+using Godot;
+using System;
+using Game.Assets.Scripts.Loading;
+using Game.Assets.Scripts.Saving;
+
+public partial class Player : Area2D
+{
+	[Export]
+	public int Speed { get; set; } = 200; // How fast the player will move (pixels/sec).
+	//[Signal]
+	//public delegate void HitEventHandler();
+	
+	public Vector2 ScreenSize;
+	
+	public string species = "Togruta";
+	public string bodyType = "1";
+	public string pChoice = "1", eChoice = "3";
+	
+public override void _Ready() //called on start
+{
+	ScreenSize = GetViewportRect().Size;
+	 Load();
+}
+
+public override void _Process(double delta) //called in real time
+{
+	var velocity = Vector2.Zero; // The player's movement vector.
+//input handlers
+	if (Input.IsActionPressed("move_right")){velocity.X += 1;}
+	if (Input.IsActionPressed("move_left")){velocity.X -= 1;}
+	if (Input.IsActionPressed("move_down")){velocity.Y += 1;}
+	if (Input.IsActionPressed("move_up")){velocity.Y -= 1;}
+
+//animation variables
+	var body = GetNode<AnimatedSprite2D>("Body");
+	var head = GetNode<AnimatedSprite2D>("Head");
+	var eye = GetNode<AnimatedSprite2D>("Eyes");
+	var hair = GetNode<AnimatedSprite2D>("Hair");
+	var pattern = GetNode<AnimatedSprite2D>("Head/Pattern");
+	var whites = GetNode<AnimatedSprite2D>("Head/Whites");
+	
+//movement and animation controls
+if (velocity.Length() > 0){
+	velocity = velocity.Normalized() * Speed;
+		
+	AnimationTurn(body, velocity, bodyType, "body");
+	body.Play();
+	AnimationTurn(head, velocity);
+	head.Play();
+	AnimationTurn(eye, velocity, eChoice, "eye");
+	eye.Play();
+	
+	//check to make sure only the ones with patterns have the node active
+	if(species == "Human" || species == "Pureblood"){
+		pattern.Hide();
+		} else {
+		pattern.Show();
+		AnimationTurn(pattern, velocity, pChoice);
+		pattern.Play();
+		if(species == "Togruta"){
+			whites.Show();
+			AnimationTurn(whites, velocity, pChoice);
+			whites.Play();
+		} else {
+			whites.Hide();
+			}
+		}
+	}
+	else{
+		body.Stop();
+		head.Stop();
+		pattern.Stop();
+		whites.Stop();
+}
+	
+	//actual thing that makes movement work
+	Position += velocity * (float)delta;
+	Position = new Vector2(
+	x: Mathf.Clamp(Position.X, 0, ScreenSize.X),
+	y: Mathf.Clamp(Position.Y, 0, ScreenSize.Y)
+	);
+//end of delta
+}
+
+/* animation swapping handler.
+@params 
+node& velocity are self explanatory, just the node you need to switch and the players movement vector
+choice deals with which ever enumerated choice of whichever thing. so like, for eye one, choice is
+"eChoice" which is currently set to 1. 
+type is where you pass in body, eye, hair,species, etc. 
+*/
+public void AnimationTurn(AnimatedSprite2D node, Vector2 velocity, string choice = "", string type = ""){
+	if(choice != ""){
+		choice = ("_"+choice);
+	}
+	if(type == ""){
+		type = species;
+	}
+		
+	if(velocity.X != 0){
+		node.Animation = (type+choice+"_side");
+		node.FlipH = velocity.X < 0;
+		
+		if(type == "eye"){ node.Show(); }
+	} else if(velocity.Y < 0){
+		if(type == "eye"){
+			node.Hide();
+		} else {
+			node.Animation = (type+choice+"_back");
+		}
+	} else{
+		node.Animation = (type+choice+"_front");
+		if(type == "eye"){ node.Show(); }
+	}
+	//end AnimationTurn()
+}
+
+public void Load()
+	{
+		using var file = FileAccess.Open("user://player_save.dat", FileAccess.ModeFlags.Read);
+		if (file is not null)
+		{
+			Position = (Vector2)file.GetVar();
+		}
+	}
+
+	public void Save()
+	{
+		using var file = FileAccess.Open("user://player_save.dat", FileAccess.ModeFlags.Write);
+		file.StoreVar(Position);
+	}
+
+//end of code <- keep right before final bracket
+}
