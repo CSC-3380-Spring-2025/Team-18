33707a252ae77600f60b64c46911ee44a5ead584
Mason Khonsari <khonsari@mason.codes>
2025-03-06 21:20:32 -0600

    Migrate saving/loading functionality to Main Menu

--- 3380-game/Assets/Scripts/MainMenu.cs
+++ 3380-game/Assets/Scripts/MainMenu.cs
@@ -1,8 +1,11 @@
 using Godot;
 using System;
+using Game.Assets.Scripts.Loading;
+using Game.Assets.Scripts.Saving;
 
 public partial class MainMenu : CanvasLayer
 {
+	private bool _isFirstLoad = true;
 	// Called when the node enters the scene tree for the first time.
 	///Makes it so the play and exit button both function as intended when esc is pressed or game is 'paused'.
 	public override void _Ready(){
@@ -23,7 +26,6 @@ public partial class MainMenu : CanvasLayer
 		GetTree().Paused = false;
 		
 		var mainChildren = GetParent().GetChildren();
-		
 		foreach(var child in mainChildren){
 			//If statement is so whenever the start button is pressed, it doesn't continously create a new node of Player scene
 			if(child.Name == "Player")
@@ -34,10 +36,31 @@ public partial class MainMenu : CanvasLayer
 		var gameScene = GD.Load<PackedScene>("res://Scenes//Player.tscn");
 		var gameSceneNode = gameScene.Instantiate();
 		GetParent().AddChild(gameSceneNode);
+		// Load elements if it is the first load and they are loadable
+		if (_isFirstLoad)
+		{
+			foreach (var child in gameSceneNode.GetChildren())
+			{
+				if (child is Loadable loadable)
+				{
+					loadable.Load();
+				}
+			}
+
+			_isFirstLoad = false;
+		}
 		
 	}
 	///Functionality for the exit button
 	public void OnExitPressed(){
+		// Save any savables on exit
+		foreach (var child in GetParent().GetChildren())
+		{
+			if (child is Savable savable)
+			{
+				savable.Save();
+			}
+		}
 		GetTree().Quit();
 	}
 }
