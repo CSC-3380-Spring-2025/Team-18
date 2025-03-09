c95f1f561b3091b7798b9ef1c32c1158b94ee69c
Jeremiah McKeey <jmcke24@lsu.edu>
2025-03-08 19:53:51 -0600

    Pause screen functionality
    Created scenes for a pause screen, as well as scenes for the following: Inventory, stats, and settings.
    Made it so when the 'esc' is pressed, instead of the main menu reappearing the pause screen appears. For now, the buttons don't work when that event happens-will fix it.

---
+++ 3380-game/Scenes/PauseScreen.cs
@@ -0,0 +1,39 @@
+using Godot;
+using System;
+
+public partial class PauseScreen : CanvasLayer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready(){
+		//GetNode<Button>("Resume").Pressed += ResumePressed;
+		//GetNode<Button>("Inventory").Pressed += InventoryPressed;
+		//GetNode<Button>("Stats").Pressed += StatsPressed;
+		//GetNode<Button>("Settings").Pressed += SettingsPressed;
+		//GetNode<Button>("Quit").Pressed += QuitPressed;
+	}
+	public void ResumePressed(){
+			Visible = false;
+			GetTree().Paused = false;
+			}
+	public void QuitPressed(){
+		GetTree().Quit();
+	}
+	public void InventoryPressed(){
+		SwitchScene("res://Scenes//inventory.tscn");
+	}
+	public void StatsPressed(){
+		SwitchScene("res://Scenes//stats.tscn");
+	}
+	public void SettingsPressed(){
+		SwitchScene("res://Scenes//settings.tscn");
+	}
+	private void SwitchScene(string scenePath){
+		//Visible = false;
+		GetTree().Paused = false;
+		GetTree().ChangeSceneToFile(scenePath);
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
+}
