738e3ec927c174211a6e1e3ad78af235e84fa5db
Mason Khonsari <khonsari@mason.codes>
2025-04-10 18:09:14 -0500

    Document AddPoints method

--- 3380-game/Assets/Scripts/Experience.cs
+++ 3380-game/Assets/Scripts/Experience.cs
@@ -8,11 +8,15 @@ public sealed class Experience(uint points)
     {
     }
     
+    /// <summary>
+    /// Adds the specified number of points to the total stored points
+    /// </summary>
+    /// <param name="points"></param>
     public void AddPoints(uint points) => Points += points;
     
     /// <summary>
     /// Converts the total number of points to a level using the inverse of our level to points formula
-    /// This inverse formula is not exact given the use of ceil and floor functions in the original, but the difference is insignificant
+    /// This inverse formula is not exact given the use of ceil and floor functions in the original formula, but the difference is negligible
     /// </summary>
     /// <returns>The level associated with the number of points in this Experience object</returns>
     public int GetLevel()
